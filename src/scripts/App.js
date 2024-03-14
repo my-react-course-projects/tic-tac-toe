86361668a9fc2338335a1874334cd9b93fe6e4f5
eshwagadkar <eshwagadkar007@gmail.com>
2024-03-14 20:47:23 +0530

    delete header component

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,10 +1,10 @@
-import React from "react"
-import Header from "./components/Header"
+import React from 'react'
+import Main from './components/Main'
 
 const App = () => {
     return (
         <>
-            <Header />
+            <Main />
         </>
     )
 }
