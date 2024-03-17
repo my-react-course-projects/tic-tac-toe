a9f497f11c3ba83953d7edb8673c6e21930cb8ef
eshwagadkar <eshwagadkar007@gmail.com>
2024-03-17 20:56:46 +0530

    2-way binding implementation

--- src/scripts/components/Player.jsx
+++ src/scripts/components/Player.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react'
 
-export default ({ name, symbol }) => {
-
+export default ({ initialName, symbol }) => {
+    const [playerName, setPlayerName] = useState(initialName)
     const [edit, setEdit] = useState(false)
-    const clickHandler = () => setEdit(prevState => !edit)
 
-    let playerName = <span className='player__name'>{name}</span>
+    const clickHandler = () => setEdit(prevState => !edit)
+    const inputChangeHandler = (event) => {
+      setPlayerName(event.target.value)
+    }
+
+    let editablePlayerName = <span className='player__name'>{playerName}</span>
     let buttonCaption = 'Edit'
 
     if(edit) { 
-      playerName = <input className='player__input' type='text' required />
+      editablePlayerName = <input className='player__input' type='text' value={playerName} onChange={inputChangeHandler} required />
       buttonCaption = 'Save'
     }
 
     return (
         <li>
           <span className="player">
-            { playerName }
+            { editablePlayerName }
             <span className='player__symbol'>{symbol}</span>
           </span>
           <button onClick={clickHandler}>{buttonCaption}</button>
--- src/scripts/layouts/Main.jsx
+++ src/scripts/layouts/Main.jsx
@@ -8,9 +8,9 @@ export default () => {
                 <div id='game-container'>
                     <ol id='players'>
                         {/* Player 1 */}
-                        <Player name='Player 1' symbol='X' />
+                        <Player initialName='Player 1' symbol='X' />
                         {/* Player 2 */}
-                        <Player name='Player 2' symbol='O' />
+                        <Player initialName='Player 2' symbol='O' />
                     </ol>
                 </div>
             </main>
